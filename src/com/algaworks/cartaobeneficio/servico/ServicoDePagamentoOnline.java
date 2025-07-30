062d033ed015fb26e935545c6274db8a46d62d5b
Uislan Cruz <uislansk8@hotmail.com>
2025-07-29 22:22:04 -0300

    Encapsulando: ocultando variaveis com o private para que não possam ser modificadas, e criando metodos para que possamos acessar as instacias das variaveis de forma correta

--- src/com/algaworks/cartaobeneficio/servico/ServicoDePagamentoOnline.java
+++ src/com/algaworks/cartaobeneficio/servico/ServicoDePagamentoOnline.java
@@ -8,13 +8,9 @@ public class ServicoDePagamentoOnline {
 
     public Recibo efetuarPagamento(Estabelecimento estabelecimento, Cartao cartao, double valor){
 
-        if(cartao.saldo < valor){
-            throw new RuntimeException("Saldo insuficiente para pagamento");
-        }
 
-        cartao.saldo -= valor;
-        // TODO realiza outras logicas para efetuar o pagamento ao estabelecimento
+        cartao.debitar(valor);
 
-        return new Recibo(cartao.titular, "Pagamento", valor);
+        return new Recibo(cartao.obterTitular(), "Pagamento", valor);
     }
 }

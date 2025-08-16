d8b87133b0e9c5e30ec0912deabd798fb4a91edf
Uislan Cruz <uislansk8@hotmail.com>
2025-08-16 14:16:55 -0300

    BoaAjustando e encapsulando a classe para proteger e melhorar o programa

--- src/com/algaworks/cartaobeneficio/agenda/Principal.java
+++ src/com/algaworks/cartaobeneficio/agenda/Principal.java
@@ -3,6 +3,7 @@ package com.algaworks.cartaobeneficio.agenda;
 public class Principal {
     public static void main(String[] args) {
         Horario horario = new Horario(10,55);
-        System.out.printf("%dh%dm", horario.hora, horario.minuto);
+
+        System.out.println(horario.formatar());
     }
 }

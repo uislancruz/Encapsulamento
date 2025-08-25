da30794721ffe05f945677d898b6cec78a31d745
Uislan Cruz <uislansk8@hotmail.com>
2025-08-25 19:49:16 -0300

    CBoas práticas: crie cópias defensivas

--- src/com/algaworks/cartaobeneficio/agenda/Principal.java
+++ src/com/algaworks/cartaobeneficio/agenda/Principal.java
@@ -2,8 +2,17 @@ package com.algaworks.cartaobeneficio.agenda;
 
 public class Principal {
     public static void main(String[] args) {
-        Horario horario = new Horario(10,55);
+        Horario horario = new Horario(10,30);
+        Agendamento agendamentoCabelo = new Agendamento(horario, "Corte de cabelo");
+        Agendamento agendamentoBarba = new Agendamento(horario, "Corte Barba");
+
+        horario.setHora(11);
+        horario.setMinuto(45);
+
+        imprimir
 
         System.out.println(horario.formatar());
     }
+
+    private static void
 }
